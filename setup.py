158651459eae96b98d5aa4a9f4d97d3e72f0b8fa
samschrb <ssschreiber@gmail.com>
2025-09-12 17:38:02 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -41,8 +41,10 @@ add_files = [
     (homedir, ["VERSION"]),
     (homedir + "web/css/inc", ["VERSION"]),
     (systemd_configdir, ["conf/systemd/rsumpwebchart.service"]),
+    (systemd_configdir, ["conf/systemd/rsumphourchart.service"])
     (systemd_configdir, ["conf/systemd/raspisump.service"]),
     (systemd_configdir, ["conf/systemd/rsumpwebchart.timer"]),
+    (systemd_configdir, ["conf/systemd/rsumphourchart.timer"]),
 ]
 
 setup(
